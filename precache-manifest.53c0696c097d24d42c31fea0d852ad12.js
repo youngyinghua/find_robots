326ac5cd01bb917f0f4b48c89d88ac4170a02c73
youngyinghua <67694333+youngyinghua@users.noreply.github.com>
2020-09-05 23:58:06 +0900

    Updates

---
+++ precache-manifest.53c0696c097d24d42c31fea0d852ad12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9635ae7c821abdcd2e58daa0c5c9439",
+    "url": "/findrobots/index.html"
+  },
+  {
+    "revision": "30e8120c108face3f42f",
+    "url": "/findrobots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "33e42cd5277b70542825",
+    "url": "/findrobots/static/css/main.6e1c16b0.chunk.css"
+  },
+  {
+    "revision": "30e8120c108face3f42f",
+    "url": "/findrobots/static/js/2.438da5e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/findrobots/static/js/2.438da5e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33e42cd5277b70542825",
+    "url": "/findrobots/static/js/main.42eac6d6.chunk.js"
+  },
+  {
+    "revision": "663cf95caec10ea5832e",
+    "url": "/findrobots/static/js/runtime-main.8ef3a6bb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/findrobots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
